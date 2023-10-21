70e1ca04eb3308c1182a3d4e3d06e6d948f848b5
AnnaShp <nyutka_slutskaya@mail.ru>
2023-10-21 16:56:39 +0300

    change carusel component and style

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,60 +1,71 @@
-import "./Carousel.css";
+import s from "./Carousel.module.css";
 import Data from "../Data/Data.json";
-import CardWords from "../Words/Words";
+import CardWords from "./Words/Words";
 import { useState } from "react";
 
 export const Carousel = (props) => {
-  const [emptyId, setTranslate] = useState(true);
-  const [offset, setOffset] = useState(0);
-  const page_widht = 300;
   // функция показа перевода слова
+  const [emptyId, setTranslate] = useState(true);
   const showTranslate = (id) => {
     setTranslate(id);
   };
-  // функция листания назад
+  // переменные для карусели
+  const [offset, setOffset] = useState(0);
+  const page_widht = 300;
+  //листание назад
   const handlePrevBtn = () => {
     setOffset((currentOffset) => {
       const newOffset = currentOffset + page_widht;
       return Math.min(newOffset, 0);
     });
   };
-  // функция листания вперед
+
+  // листание вперед
+  const maxOffset = -(page_widht * (Data.length - 1));
   const handleNextBtn = () => {
     setOffset((currentOffset) => {
       const newOffset = currentOffset - page_widht;
-      const maxOffset = -(page_widht * (Data.length - 1));
       return Math.max(newOffset, maxOffset);
     });
   };
 
   return (
-    <div className="carousel" id="cards">
-      <button className="btn_prev" onClick={handlePrevBtn}>
-        Назад
-      </button>
-      <div className="card_container">
-        <div
-          className="card_visible"
-          style={{ transform: `translateX(${offset}px)` }}
-        >
-          {Data.map((word) => (
-            <CardWords
-              key={word.id}
-              id={word.id}
-              eng={word.eng}
-              transcription={word.transcription}
-              rus={word.rus}
-              topic={word.topic}
-              emptyId={emptyId}
-              setTranslate={setTranslate}
-              showTranslate={showTranslate}
-            />
-          ))}
+    <div className={s.wrapper}>
+      <div className={s.carousel} id={s.cards}>
+        <button className={s.btn_prev} onClick={handlePrevBtn}>
+          Назад
+        </button>
+        <div className={s.card_container}>
+          <div
+            className={s.card_visible}
+            style={{ transform: `translateX(${offset}px)` }}
+          >
+            {Data.map((word) => (
+              <CardWords
+                key={word.id}
+                id={word.id}
+                eng={word.eng}
+                transcription={word.transcription}
+                rus={word.rus}
+                topic={word.topic}
+                emptyId={emptyId}
+                setTranslate={setTranslate}
+                showTranslate={showTranslate}
+              />
+            ))}
+          </div>
         </div>
+        <button className={s.btn_next} onClick={handleNextBtn}>
+          Дальше
+        </button>
       </div>
-      <button className="btn_next" onClick={handleNextBtn}>
-        Дальше
-      </button>
+      <div className={s.error}>
+        {
+          <span style={{ display: offset === maxOffset ? "flex" : "none" }}>
+            Карточки закончились, но можно начать сначала!
+          </span>
+        }
+      </div>
     </div>
   );
 };
