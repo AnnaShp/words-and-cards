d5b7866732644f65f72789350d6eb4b29d734d8a
AnnaShp <nyutka_slutskaya@mail.ru>
2023-10-21 16:58:02 +0300

    update app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
-import Header from "./components/Header/Header";
+import HeaderNav from "./components/Header/Header";
+import Home from "./components/Home/Home";
 import { Carousel } from "./components/Carousel/Carousel";
 import BasicTable from "./components/ReactTable/ReactTable";
+import EmptyPage from "./components/EmptyPage/Empty";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Carousel />
-      <BasicTable />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <HeaderNav />
+        <div className="main_part">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/cards" element={<Carousel />}></Route>
+            <Route path="/table" element={<BasicTable />}></Route>
+            <Route path="*" element={<EmptyPage />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
